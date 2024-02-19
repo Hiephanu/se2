0c3f28467a6fcbc97acc18b4ba4f6ec1fd9ea7e1
Hiephanu <doanvanhiep230903@gmail.com>
2024-02-19 22:14:13 +0700

    update project structure

---
+++ src/main/java/com/example/se2/post/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.example.se2.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PostController {
+    @RequestMapping("/")
+    public String test(Model model){
+        model.addAttribute("message","Hello how are you?");
+        return "Home";
+    }
+    @RequestMapping("/about")
+    public String About(){
+        return "About";
+    }
+}
